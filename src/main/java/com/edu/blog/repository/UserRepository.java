dc04180e3b7210663c24b3c9e65dd33646311a48
simhw <simhw96@gmail.com>
2022-01-01 16:17:03 +0900

    Select User 객체 (JPA findById)

--- src/main/java/com/edu/blog/repository/UserRepository.java
+++ src/main/java/com/edu/blog/repository/UserRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.edu.blog.model.User;
 
+// User 형의 Primary key 는 Integer 형이다.
 public interface UserRepository extends JpaRepository<User, Integer>{
 }
--- src/main/java/com/edu/blog/test/DummyControllerTest.java
+++ src/main/java/com/edu/blog/test/DummyControllerTest.java
@@ -1,20 +1,13 @@
 package com.edu.blog.test;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Supplier;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edu.blog.model.RoleType;
@@ -35,5 +28,31 @@ public class DummyControllerTest {
 		userRepository.save(user);
 
 		return "회원가입이 완료되었습니다.";
+	}
+
+	@GetMapping("/dummy/user/{idx}")
+	public User selectUser(@PathVariable int idx) {
+
+
+//		User user = userRepository.findById(idx).orElseThrow(new Supplier<IllegalArgumentException>() {
+//
+//			@Override
+//			public IllegalArgumentException get() {
+//				return new IllegalArgumentException("해당 회원이 존재하지 않습니다.");
+//			}
+//
+//		});
+
+		User user =  userRepository.findById(idx).orElseThrow(() -> {
+			return new IllegalArgumentException("해당 회원이 존재하지 않습니다.");
+		});
+
+		return user;
+	}
+
+	@GetMapping("/dummy/user")
+	public List<User> selectAllUser() {
+		return userRepository.findAll();
+
 	}
 }
