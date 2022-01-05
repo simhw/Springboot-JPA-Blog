c9624379a87ee79196d648fb26c0138bf9ba93c7
simhw <simhw96@gmail.com>
2022-01-06 00:13:16 +0900

    ExceptionHandler(IllegalArgumentException) 추가

--- src/main/java/com/edu/blog/handler/GlobalExceptionHandler.java
+++ src/main/java/com/edu/blog/handler/GlobalExceptionHandler.java
@@ -12,8 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(value = Exception.class)
+    public ResponseDto<String> handleException(Exception ex) {
+        return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage());
+    }
+
+    // 예외 처리마다 클래스 생성이 가능하다.
     @ExceptionHandler(value = IllegalArgumentException.class)
-    public ResponseDto<String> handleArgumentException(IllegalArgumentException ex) {
+    public ResponseDto<String> handleIllegalArgumentException(IllegalArgumentException ex) {
         return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage());
     }
 }
