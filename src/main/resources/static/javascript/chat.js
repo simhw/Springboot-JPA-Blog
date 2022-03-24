f5a42a5a201fa4ba73a3b7c56ef06429391ab019
simhw <simhw96@gmail.com>
2022-03-25 03:13:59 +0900

    채팅 메세지 user 정보 포함 저장

--- src/main/resources/static/javascript/chat.js
+++ src/main/resources/static/javascript/chat.js
@@ -1,57 +1,59 @@
-function getSendMsgContent(text, time) {
+// 보낸 메세지
+function getSendMsgContent(data) {
     return `<div class="media-body">
-                <p>${text}</p>
-                <p class="meta"><time datetime="2022">${time}</time></p>            
+                <p>${data.msg}</p>
+                <p class="meta"><time datetime="2022">${data.createdAt}</time></p>            
             </div>`;
 }
 
-// 기존 메세지 생성
-function initChatContent(message, createdAt) {
-    let date = new Date(createdAt);
-    let time = date.getHours() + ":" + date.getMinutes();
+// 받은 메세지
+function getReceiveMsgContent(data) {
+    return `<div class="media media-chat">
+                 <img src="/image/chat_profile.png">
+                 <div class="media-body">
+                    <p>${data.msg}</p>
+                    <p class="meta"><time datetime="2022">23:58</time></p>
+                 </div>
+             </div>`;
+}
 
+// 초기화
+// 기존 메세지 생성
+function initChatContent(data) {
     let chat = document.querySelector("#chat-content");
     let chatOutgoingContent = document.createElement("div");
     chatOutgoingContent.className = "media media-chat media-chat-reverse";
-    chatOutgoingContent.innerHTML = getSendMsgContent(message, time);
+    chatOutgoingContent.innerHTML = getSendMsgContent(data);
     chat.append(chatOutgoingContent);
 }
 
+// 메세지 전송 및 저장
 // 새로운 메세지 생성
 function addChatContent() {
-    let chatContent = document.querySelector('#chat-content');
     let inputTxt = document.querySelector('#chat-outgoing-txt');
-    let chatOutgoingContent = document.createElement('div');
-    chatOutgoingContent.className = "media media-chat media-chat-reverse";
-
-    let date = new Date();
-    let time = date.getHours() + ":" + date.getMinutes();
-
-    // 메세지 전송 및 저장
+    let user = document.getElementById('user');
     let chat = {
-        sender: "sim",
+        sender: user.value,
         receiver: "kim",
+        room: 1,
         msg: inputTxt.value,
-        createdAt: date
     };
+    console.log(chat);
 
     fetch("http://localhost:9090/chat", {
         method: "POST",
         headers: {"Content-Type": "application/json; charset=utf-8"},
         body: JSON.stringify(chat),
-    }).then(function (response) {
-        console.log(response)
     });
-
-    chatOutgoingContent.innerHTML = getSendMsgContent(inputTxt.value, time);
-    chatContent.append(chatOutgoingContent);
+    console.log(chat);
     inputTxt.value = "";
 }
-// 기존 메세지 출력
-const eventSource = new EventSource("http://localhost:9090/sender/sim/receiver/kim");
+
+// SSE 서버 연결 및 기존 메세지 출력
+const eventSource = new EventSource("http://localhost:9090/chat/room/1");
 eventSource.onmessage = function (event) {
     const data = JSON.parse(event.data);
-    initChatContent(data.msg, data.createdAt)
+    initChatContent(data);
 }
 
 // Press Button
@@ -62,7 +64,6 @@ document.querySelector('#chat-outgoing-btn').addEventListener("click", function 
 
 // Enter
 // 새로운 메세지 출력
-
 document.querySelector('#chat-outgoing-txt').addEventListener("keydown", function (key) {
     if (key.keyCode === 13) {
         addChatContent();
--- src/main/webapp/WEB-INF/views/chat/chatForm.jsp
+++ src/main/webapp/WEB-INF/views/chat/chatForm.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <%@ include file="../layout/header.jsp" %>
-
+<input type="hidden" id="user" value="${principal.user.idx}"/>
 <div class="container">
     <div class="row container d-flex justify-content-center">
         <div class="col-md-6">
